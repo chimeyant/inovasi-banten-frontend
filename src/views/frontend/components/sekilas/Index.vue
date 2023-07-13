07195bdc6555cd2368442628f9bbf82a12ae2ea9
chimey <selamet.antsoftmedia@gmail.com>
2023-07-13 09:33:17 +0700

    fix frontend

--- src/views/frontend/components/sekilas/Index.vue
+++ src/views/frontend/components/sekilas/Index.vue
@@ -4,17 +4,27 @@
     <div class="mt-10">
       <v-container>
         <v-row>
-          <v-col cols="5">
-            <span>GAMBAR ILUSTRASI</span>
-          </v-col>
-          <v-col cols="7">
+          <v-col cols="6">
+            <v-row class="justify-content-center">
+              <img
+                src="images/ilustration02.png"
+                alt=""
+                srcset=""
+                width="450"
+              >
 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolorum fuga magni ipsa impedit vel perferendis dicta veniam atque, provident sequi maxime ad expedita optio qui quidem minus rem. Id?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur amet itaque aliquam dolor illo. Nulla harum quibusdam provident repellat, minus ipsam ab debitis non perferendis! Adipisci culpa aliquam ipsum nisi!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perferendis deserunt nostrum animi nemo minima quas. Iusto, dignissimos molestiae? Magnam fugiat odio distinctio dolor dolore minima, unde dicta quos quas!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate minima labore saepe quidem animi amet expedita ad atque consectetur! Repellat odio voluptate aperiam aliquam vitae est rerum illum ex fugit.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe ducimus nesciunt tempora itaque ratione quae vitae laborum quibusdam soluta quod blanditiis nobis, quasi est maxime officiis amet at totam.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit quasi esse voluptatem nemo doloribus molestias nostrum maxime. Doloribus dignissimos repudiandae dicta saepe commodi veniam architecto cumque at fugit, tenetur sint!
+            </v-row>
+
+          </v-col>
+          <v-col cols="6">
+            <p style="text-align: justify;">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolorum fuga magni ipsa impedit vel perferendis dicta veniam atque, provident sequi maxime ad expedita optio qui quidem minus rem. Id?
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur amet itaque aliquam dolor illo. Nulla harum quibusdam provident repellat, minus ipsam ab debitis non perferendis! Adipisci culpa aliquam ipsum nisi!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perferendis deserunt nostrum animi nemo minima quas. Iusto, dignissimos molestiae? Magnam fugiat odio distinctio dolor dolore minima, unde dicta quos quas!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate minima labore saepe quidem animi amet expedita ad atque consectetur! Repellat odio voluptate aperiam aliquam vitae est rerum illum ex fugit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe ducimus nesciunt tempora itaque ratione quae vitae laborum quibusdam soluta quod blanditiis nobis, quasi est maxime officiis amet at totam.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit quasi esse voluptatem nemo doloribus molestias nostrum maxime. Doloribus dignissimos repudiandae dicta saepe commodi veniam architecto cumque at fugit, tenetur sint!
+            </p>
           </v-col>
         </v-row>
 
