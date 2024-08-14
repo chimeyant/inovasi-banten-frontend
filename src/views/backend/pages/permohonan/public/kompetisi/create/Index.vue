856715f4ec6ac00751f62f8b39a4e39e32a35eb8
chimey <selamet.antsoftmedia@g.gmail.com>
2024-08-14 22:00:54 +0700

    fix

--- src/views/backend/pages/permohonan/public/kompetisi/create/Index.vue
+++ src/views/backend/pages/permohonan/public/kompetisi/create/Index.vue
@@ -796,7 +796,7 @@ export default {
       Paragraph,
     ],
     kelompoks: [
-      { text: "Umum", value: "umum" },
+      { text: "Umum", value: "umum", disabled: true },
       { text: "Khusus", value: "khusus" },
     ],
 
